c6c0683b7cebe521599503777d9598aa2149ec1e
CJ May <cmtechboy1@gmail.com>
2020-02-15 05:26:07 -0500

    added working api call to azure text services

--- HackMeDaddy/app.js
+++ HackMeDaddy/app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const CognitiveServicesCredentials = require('ms-rest-azure').CognitiveServicesCredentials;
+const ContentModeratorAPIClient = require('azure-cognitiveservices-contentmoderator');
 
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
+//var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -21,10 +24,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
+// For use with web app and api call from field input
+indexRouter.post('/api/grabtext', function(req, res){
+  console.log("in /api/grabtext");
+  const profile = req.body.profile;
+  console.log(profile);
+
+})
+
 indexRouter.post('/api/text', function(req, res) {
   console.log("received request from extension!: " + req.body.text);
-  res.json({message: 'hooray!'});
+  let creds = new CognitiveServicesCredentials("08b28c9251ea4c0fb88f4fff6044f350");
+  let client = new ContentModeratorAPIClient(creds, "https://tartanhackstest.cognitiveservices.azure.com/");
+  client.textModeration.screenText('text/plain', req.body.text, (err, result, req, res) => {
+  if (err) throw err;
+  console.log(result.pII);
+  });
+
+  res.json([result.pII]);
 });
+// app.use('/api/text/', function(req, res) {
+//   console.log("received request from extension!: " + req.body.text);
+//   console.log("sending off " + JSON.stringify({message: 'hooray!'}));
+//   res.status(200).json({"message": 'hooray!'});
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- HackMeDaddy/routes/api.js
+++ HackMeDaddy/routes/api.js
@@ -4,11 +4,13 @@ var router = express.Router();
 var api_controller = require('../controllers/apiController')
 
 // POST profile URL to Facebook for text analysis
-router.post('/grabFB/', );
+router.post('/grabFB/', api_controller.grabFB);
 
 // POST from extension to send text to Azure for analysis
-router.post('/analyze-text/');
+router.post('/analyze-text/', api_controller.analyze_text);
 
-router.post('/analyze-image/');
+// POST from extension to send image to Azure for analysis
+// will respond to 
+router.post('/analyze-image/', api_controller.analyze_image);
 
 module.exports = router;
